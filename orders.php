63773d04ec437af7f18e07fc31272d40ac63b348
Claudio Benvenuti <claudio@benve.it>
2021-09-17 15:39:59 +0200

    orders

---
+++ orders.php
@@ -0,0 +1,25 @@
+<?php
+if(!isset($_COOKIE["user_id"])){
+  header('Location: login.php');
+}
+?>
+
+<h1>Ordini</h1>
+<?php
+
+$con = mysqli_connect("localhost", "ecommerce", "cicciobello", "my_ecommerce");
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+$result = mysqli_query($con, "SELECT * FROM orders WHERE user_id = " . $_COOKIE["user_id"]);
+
+while($row = $result->fetch_assoc()){
+  echo "<div>Numero:" . $row['order_number'] . "<br>Note:" . $row['notes'] . "</div>";
+  echo "<hr>";
+}
+echo "<br>";
+
+?>
+<div><a href="area-riservata.php">Indietro</a></div>
+<hr>
+<p><small>Copyright 2021</small></p>
