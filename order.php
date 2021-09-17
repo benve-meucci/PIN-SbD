aeadbd2bec3542af9f20a07dc6ec8144b0bdc2e1
Claudio Benvenuti <claudio@benve.it>
2021-09-17 16:01:53 +0200

    order

--- order.php
+++ order.php
@@ -4,26 +4,22 @@ if(!isset($_COOKIE["user_id"])){
 }
 ?>
 
-<h1>Ordini</h1>
+<h1>Dettagli ordine</h1>
 <?php
 
 $con = mysqli_connect("localhost", "ecommerce", "cicciobello", "my_ecommerce");
 if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
 }
-$result = mysqli_query($con, "SELECT * FROM orders WHERE user_id = " . $_COOKIE["user_id"]);
+$result = mysqli_query($con, "SELECT * FROM orders WHERE id = " . $_GET['id']);
+$row = $result->fetch_assoc();
+?>
+<div>ID: <?php echo($row['id']) ?> </div>
+<div>Numero: <?php echo($row['order_number']) ?> </div>
+<div>Notes: <?php echo($row['notes']) ?> </div>
+<hr>
 
-while($row = $result->fetch_assoc()){
-?>
-  <div>Numero: 
-  <a href="order.php?id=<?php echo $row['id'] ?>">
-    <?php echo($row['order_number']); ?>
-  </a>
-</div>
-  <hr>
-<?php
-}
-?>
+<div><a href="orders.php">Ordini</a></div>
 <div><a href="area-riservata.php">Dashboard</a></div>
 <hr>
 <p><small>Copyright 2021</small></p>
